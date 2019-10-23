4b792c8106b7b801846427d2620eab59abbb5a34
simantini <keysaanproj@gmail.com>
2019-10-23 14:11:05 +0530

    commit login farmer by simantini

--- Scheme/src/main/java/com/lti/interfaces/SellRequestInterface.java
+++
@@ -1,9 +0,0 @@
-package com.lti.interfaces;
-
-import com.lti.entity.SellRequest;
-
-public interface SellRequestInterface {
-	
-	public void addSellRequest(SellRequest sellRequest);
-	public void removeSellRequest(SellRequest sellRequest);
-}
